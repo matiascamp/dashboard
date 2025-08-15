9f0ba91eca3fd437839025cff440bef7d54e2456
matias <you@example.com>
2025-08-15 02:25:54 -0300

    Fix query to proper work in postgres

--- src/app/api/summaries/route.ts
+++ src/app/api/summaries/route.ts
@@ -3,31 +3,31 @@ import { NextResponse } from 'next/server'
 
 export const GET = async () => {
   try {
-    
+
     const resumen = await prisma.$queryRaw<{
-        year: bigint
-        month: bigint
-        debit: number
-        havings: number
-      }[]>`
-      SELECT
-        YEAR(date) AS year,
-        MONTH(date) AS month,
+      year: bigint
+      month: bigint
+      debit: number
+      havings: number
+    }[]>`
+        SELECT
+        EXTRACT(YEAR FROM date) AS year,
+        EXTRACT(MONTH FROM date) AS month,
         SUM(CASE WHEN type = 'outcoming' THEN amount ELSE 0 END) AS debit,
         SUM(CASE WHEN type = 'incoming' THEN amount ELSE 0 END) AS havings
-      FROM MovementDetail
+      FROM "MovementDetail"
       GROUP BY year, month
       ORDER BY year DESC, month DESC
       `
 
-      const resumenConvertido = resumen.map(item => ({
-        year: Number(item.year),
-        month: Number(item.month),
-        debit: item.debit,
-        havings: item.havings,
-      }))
-      
-  return NextResponse.json(resumenConvertido)
+    const resumenConvertido = resumen.map(item => ({
+      year: Number(item.year),
+      month: Number(item.month),
+      debit: item.debit,
+      havings: item.havings,
+    }))
+
+    return NextResponse.json(resumenConvertido)
   } catch (error) {
     console.error('Error al intentar obtener asiento contable:', error)
     return NextResponse.json({ error: ' al intentar obtener asiento contable:' }, { status: 500 })
