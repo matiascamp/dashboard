4660b2a83c4b080f8947f86974dc786bdd5e163b
matiascamp <campodonicomatias@outlook.com>
2025-09-03 01:26:52 -0300

    Fix matcher in middleware to avoid images

--- src/middleware.ts
+++ src/middleware.ts
@@ -37,5 +37,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: '/((?!_next/static|_next/image|favicon.ico).*)'
+  matcher: '/((?!_next/static|_next/image|favicon.ico|.*\\.(jpg|jpeg|png|gif|svg)).*)'
 }
